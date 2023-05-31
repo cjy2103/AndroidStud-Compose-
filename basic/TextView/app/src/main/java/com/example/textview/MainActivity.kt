2410df1b4fd7b0ffc5c1e9d6bfe1711ad263049b
jy.choi <cjy2103@naver.com>
2023-05-31 17:52:19 +0900

    Compose TextView 부분 작성중

--- basic/TextView/app/src/main/java/com/example/textview/MainActivity.kt
+++ basic/TextView/app/src/main/java/com/example/textview/MainActivity.kt
@@ -4,12 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.textview.ui.theme.TextViewTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,14 +40,18 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier.padding(start = 10.dp, top = 20.dp),
+        fontSize = 20.dp
     )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     TextViewTheme {
         Greeting("Android")
     }
 }
+
+@Composable
+fun dpToSp(dp: Dp) = with(LocalDensity.current) { dp.toSp() }
