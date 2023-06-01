4d6351cd21bf132c646968541aeec2aacbe31c08
jy.choi <cjy2103@naver.com>
2023-06-01 11:19:25 +0900

    안드로이드(Android) 컴포즈 기초 - TextView 포스팅 완

--- basic/TextView/app/src/main/java/com/example/textview/MainActivity.kt
+++ basic/TextView/app/src/main/java/com/example/textview/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.textview
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
@@ -12,7 +13,12 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.fontResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -38,11 +44,36 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier.padding(start = 10.dp, top = 20.dp),
-        fontSize = 20.dp
+
+    val mapleFont = FontFamily(
+        Font(R.font.font_maple)
     )
+
+    val mapleLight = FontFamily(
+        Font(R.font.maplestory_light)
+    )
+
+    Column(modifier = modifier.padding(start = 20.dp, top = 20.dp, end = 20.dp, bottom = 20.dp)) {
+        Text(
+            text = "Hello $name!",
+            fontSize = dpToSp(dp = 20.dp)
+        )
+        Text(
+            text = "메이플스토리 볼드체",
+            modifier = modifier.padding(top = 20.dp),
+            fontSize = dpToSp(dp = 20.dp),
+            color = Color(0xFFFF5722),
+            fontFamily = mapleFont
+        )
+        Text(
+            text = "메이플스토리 라이트체",
+            modifier = modifier.padding(top = 20.dp),
+            fontSize = dpToSp(dp = 20.dp),
+            color = Color(0xFF03A9F4),
+            fontFamily = mapleLight
+        )
+
+    }
 }
 
 @Preview(showBackground = true, showSystemUi = true)
@@ -55,3 +86,5 @@ fun GreetingPreview() {
 
 @Composable
 fun dpToSp(dp: Dp) = with(LocalDensity.current) { dp.toSp() }
+
+
