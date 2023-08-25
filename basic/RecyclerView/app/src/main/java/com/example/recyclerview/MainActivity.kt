b7dd4f531f712a3ab5c786b0e96365d6e9d9b658
jy.choi <cjy2103@naver.com>
2023-08-25 23:42:41 +0900

    1차 리사이클러뷰 출력 완

--- basic/RecyclerView/app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ basic/RecyclerView/app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -3,9 +3,13 @@ package com.example.recyclerview
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -35,13 +39,44 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Column(modifier = modifier.padding(top = 20.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+    Column(
+        modifier = modifier.padding(top = 20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
         Text(
             text = "$name",
             modifier = modifier
         )
     }
 
+    Column(
+        modifier = Modifier.padding(top = 60.dp) // 첫 번째 Column의 가로 너비만큼 차지
+    ) {
+        FruitList()
+    }
+
+}
+
+@Composable
+fun FruitList() {
+    val fruits = listOf("사과","바나나","키위","망고","수박")
+
+    LazyColumn(contentPadding = PaddingValues(horizontal = 20.dp, vertical = 20.dp)
+    ){
+        items(fruits){ fruit ->
+            FruitItem(name = fruit)
+        }
+    }
+}
+
+@Composable
+fun FruitItem(name: String){
+    Column(modifier = Modifier.padding(top = 10.dp))
+    {
+        Text(
+            text = name
+        )
+    }
 }
 
 @Preview(showBackground = true, showSystemUi = true)
