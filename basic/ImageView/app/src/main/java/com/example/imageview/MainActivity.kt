5d2c9268c72d598b1ecf2016c5d5ced218e24616
DESKTOP-JQ763B1\cjy2103 <cjy2103@naver.com>
2023-08-25 16:56:18 +0900

    안드로이드(Android) 컴포즈(Compose) 기초 - 이미지 뷰(ImageView) ( v )

--- basic/ImageView/app/src/main/java/com/example/imageview/MainActivity.kt
+++ basic/ImageView/app/src/main/java/com/example/imageview/MainActivity.kt
@@ -3,13 +3,24 @@ package com.example.imageview
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.imageview.ui.theme.ImageViewTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,13 +42,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(modifier = modifier.padding(top = 20.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            text = "ImageView 테스트"
+        )
+
+        Spacer(modifier = Modifier.height(50.dp))
+
+        Image(
+            painterResource(id = R.drawable.baknana),
+            contentDescription  = "박나나",
+            modifier = Modifier
+                .width(250.dp)
+                .height(250.dp)
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     ImageViewTheme {
