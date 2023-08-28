7254bea50f16ff51c72dcd8a3539b2c4b2f146af
DESKTOP-JQ763B1\cjy2103 <cjy2103@naver.com>
2023-08-28 17:55:00 +0900

    저장

--- RecyclerView/CustomRecyclerView/app/src/main/java/com/example/customrecyclerview/MainActivity.kt
+++ RecyclerView/CustomRecyclerView/app/src/main/java/com/example/customrecyclerview/MainActivity.kt
@@ -3,14 +3,20 @@ package com.example.customrecyclerview
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.customrecyclerview.ui.theme.CustomRecyclerViewTheme
+import java.math.BigDecimal
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting("RecyclerView")
                 }
             }
         }
@@ -31,16 +37,27 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(
+        modifier = modifier.padding(top = 20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "$name",
+            modifier = modifier
+        )
+    }
+
+    Column {
+
+    }
+
+
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     CustomRecyclerViewTheme {
-        Greeting("Android")
+        Greeting("RecyclerView")
     }
 }
