fdd3f9785e9d5931990f038a255c3f4b6d845980
DESKTOP-JQ763B1\cjy2103 <cjy2103@naver.com>
2023-08-29 18:01:16 +0900

    저장

--- RecyclerView/CustomRecyclerView/app/src/main/java/com/example/customrecyclerview/MainActivity.kt
+++ RecyclerView/CustomRecyclerView/app/src/main/java/com/example/customrecyclerview/MainActivity.kt
@@ -6,10 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
@@ -80,16 +84,22 @@ fun CharacterList(){
         }
     }
 
+
 }
 
 @Composable
 fun CharacterItem(character: Character){
-    Column(
+    Row(
         modifier = Modifier.fillMaxWidth()
     ){
         Image(painter = painterResource(id = character.image),
-            contentDescription = stringResource(id = character.title))
+            contentDescription = stringResource(id = character.title),
+            modifier = Modifier
+                .width(150.dp)
+                .height(150.dp)
+        )
         Text(text = stringResource(id = character.title))
+        Spacer(modifier = Modifier.height(20.dp))
         Text(text = stringResource(id = character.describe))
     }
 }
