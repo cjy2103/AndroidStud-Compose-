b007f811d62a7f3e6b71f28b9233225e5cffc3e2
DESKTOP-JQ763B1\cjy2103 <cjy2103@naver.com>
2023-08-31 12:15:49 +0900

    저장

--- RecyclerView/CustomRecyclerView/app/src/main/java/com/example/customrecyclerview/MainActivity.kt
+++ RecyclerView/CustomRecyclerView/app/src/main/java/com/example/customrecyclerview/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -16,6 +18,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -23,10 +26,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.customrecyclerview.data.Character
 import com.example.customrecyclerview.data.CharacterProvider
 import com.example.customrecyclerview.ui.theme.CustomRecyclerViewTheme
@@ -75,7 +82,8 @@ fun CharacterList(){
     val characterState = CharacterProvider().getCharacterList()
 
     LazyColumn(
-        modifier = Modifier.fillMaxHeight(),
+        modifier = Modifier
+            .fillMaxHeight(),
         contentPadding = PaddingValues(horizontal = 20.dp, vertical = 20.dp)
     )
     {
@@ -89,21 +97,63 @@ fun CharacterList(){
 
 @Composable
 fun CharacterItem(character: Character){
-    Row(
-        modifier = Modifier.fillMaxWidth()
-    ){
-        Image(painter = painterResource(id = character.image),
-            contentDescription = stringResource(id = character.title),
-            modifier = Modifier
-                .width(150.dp)
-                .height(150.dp)
-        )
-        Text(text = stringResource(id = character.title))
-        Spacer(modifier = Modifier.height(20.dp))
-        Text(text = stringResource(id = character.describe))
-    }
-}
 
+        Row(
+            modifier = Modifier.fillMaxWidth()
+                .border(
+                    brush = Brush.horizontalGradient(listOf(Color.Green, Color.Blue)),
+                    width = 1.dp,
+                    shape = RectangleShape
+                )
+        ){
+            Image(painter = painterResource(id = character.image),
+                contentDescription = stringResource(id = character.title),
+                modifier = Modifier
+                    .width(100.dp)
+                    .height(100.dp)
+                    .border(1.dp, Color(0xFFBABABA))
+            )
+
+            Column(modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 20.dp, start = 20.dp, end = 20.dp)){
+                Text(
+                    text = stringResource(id = character.title),
+                    fontSize = 24.sp
+                )
+                Text(
+                    text = stringResource(id = character.describe),
+                    fontSize = 18.sp
+                )
+            }
+        }
+
+
+}
+//    Row(
+//        modifier = Modifier.fillMaxWidth()
+//    ){
+//        Image(painter = painterResource(id = character.image),
+//            contentDescription = stringResource(id = character.title),
+//            modifier = Modifier
+//                .width(100.dp)
+//                .height(100.dp)
+//        )
+//
+//        Column(modifier = Modifier
+//            .fillMaxWidth()
+//            .padding(top = 20.dp, start = 20.dp, end = 20.dp)){
+//            Text(
+//                text = stringResource(id = character.title),
+//                fontSize = 24.sp
+//            )
+//            Text(
+//                text = stringResource(id = character.describe),
+//                fontSize = 18.sp
+//            )
+//        }
+//
+//    }
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
