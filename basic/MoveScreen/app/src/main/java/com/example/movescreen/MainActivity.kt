982ac3499da724d6839abed42e141bd169e05d63
DESKTOP-JQ763B1\cjy2103 <cjy2103@naver.com>
2023-09-06 17:56:44 +0900

    이미지 저장

--- basic/MoveScreen/app/src/main/java/com/example/movescreen/MainActivity.kt
+++ basic/MoveScreen/app/src/main/java/com/example/movescreen/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.movescreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
@@ -30,17 +31,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(modifier: Modifier = Modifier) {
+    Column(modifier = Modifier.fillMaxSize()) {
+
+    }
+
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     MoveScreenTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
