4c77433ec5ad5194b53913370cc755967938e456
S120916\최재영 <cjy2103@naver.com>
2024-02-06 15:41:25 +0900

    안드로이드(Android) 컴포즈(Compose) - AlertDialog ( v )

--- basic/Dialog/app/src/main/java/com/example/dialog/MainActivity.kt
+++ basic/Dialog/app/src/main/java/com/example/dialog/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.example.dialog
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.dialog.ui.theme.DialogTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,14 +40,46 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(modifier: Modifier = Modifier) {
-    Text(
-        text = "Dialog 예제",
-        modifier = modifier
-    )
+fun Greeting() {
+
+    val context = LocalContext.current
+
+    Column(modifier = Modifier
+        .padding(top = 50.dp)
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Dialog 예제",
+        )
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Button(onClick = {
+            val builder = AlertDialog.Builder(context)
+
+            builder.setTitle("AlertDialog 예제")
+
+            builder.setMessage("이곳에 내용이 표시됩니다.")
+
+            builder.setPositiveButton("확인") { dialog, _ ->
+                dialog.dismiss()
+            }
+
+            builder.setNegativeButton("취소") { dialog, _ ->
+                dialog.dismiss()
+            }
+
+            builder.create().show()
+
+        }) {
+            Text(text = "AlertDialog 표시")
+        }
+    }
+
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     DialogTheme {
