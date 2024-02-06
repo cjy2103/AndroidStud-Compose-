e43a002e9dc8a2abdf9440338900a32ed9f5b6af
S120916\최재영 <cjy2103@naver.com>
2024-02-06 11:00:46 +0900

    ZoomIn,ZoomOut 기능 구현 완

--- basic/Zoom/app/src/main/java/com/example/zoom/MainActivity.kt
+++ basic/Zoom/app/src/main/java/com/example/zoom/MainActivity.kt
@@ -7,27 +7,19 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.clipToBounds
-import androidx.compose.ui.draw.scale
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextOverflow.Companion.Clip
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.zoom.R.drawable.tamtam
 import com.example.zoom.ui.theme.ZoomTheme
 
@@ -50,110 +42,91 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting() {
-    val scale = remember { mutableFloatStateOf(1f) }
-    val offsetX = remember { mutableFloatStateOf(0f) }
-    val offsetY = remember { mutableFloatStateOf(0f) }
-    val maxZoom = 2f
-    val minZoom = 0.5f
-    val rotationState = remember { mutableFloatStateOf(1f) }
 
+    var zoom by remember { mutableFloatStateOf(1f) }
+    var offsetX by remember { mutableFloatStateOf(0f) }
+    var offsetY by remember { mutableFloatStateOf(0f) }
+    val minScale = 0.5f
+    val maxScale = 2f
 
+    val imageWidth = painterResource(id = tamtam).intrinsicSize.width
+    val imageHeight = painterResource(id = tamtam).intrinsicSize.height
 
-//    Box {
-//        Image(painter = painterResource(id = tamtam)
-//            , contentDescription = "탬탬버린"
-//            , modifier = Modifier
-//                .fillMaxSize()
-//                .scale(scale.floatValue)
-//                .offset(offsetX.floatValue.dp, offsetY.floatValue.dp)
-//        )
-//    }
-//
-
-
-//
-//    Box(
-//        modifier = Modifier
-//            .fillMaxSize()
-//            .padding(16.dp)
-//            .pointerInput(Unit) {
-//                detectTransformGestures { _, pan, zoom, rotation ->
-//                    scale.floatValue *= zoom
-//                    rotationState.floatValue += rotation
-//                    if (scale.floatValue > maxZoom) {
-//                        scale.floatValue = maxZoom
-//                    } else if(scale.floatValue < minZoom){
-//                        scale.floatValue = minZoom
-//                    }
-//                    offsetX.floatValue += pan.x
-//                    offsetY.floatValue += pan.y
-//                }
-//
-//            }
-//    ) {
-//            Image(painter = painterResource(id = tamtam)
-//                , contentDescription = "탬탬버린"
-//                , modifier = Modifier
-//                    .fillMaxSize()
-//                    .scale(scale.floatValue)
-//                    .offset(offsetX.floatValue.dp, offsetY.floatValue.dp)
-//            )
-//    }
-
-    Box(
+    Box (
         modifier = Modifier
-            .clip(RectangleShape) // Clip the box content
-            .fillMaxSize() // Give the size you want...
+            .fillMaxSize()
             .pointerInput(Unit) {
-                detectTransformGestures { centroid, pan, zoom, rotation ->
-                    scale.value *= zoom
-                    rotationState.value += rotation
-                }
+                detectTransformGestures(
+                    onGesture = { _, pan, gestureZoom, _ ->
+                        zoom = (zoom * gestureZoom).coerceIn(minScale, maxScale)
+                        if (zoom > 1) {
+                            val maxOffsetX = imageWidth + 50f
+                            val maxOffsetY = imageHeight
+                            offsetX += pan.x * zoom
+                            offsetY += pan.y * zoom
+                            offsetX = offsetX.coerceIn(-maxOffsetX, maxOffsetX)
+                            offsetY = offsetY.coerceIn(-maxOffsetY, maxOffsetY)
+                        } else {
+                            offsetX = 0f
+                            offsetY = 0f
+                        }
+                    }
+                )
             }
-    ) {
+    ){
         Image(
+            painter = painterResource(id = tamtam),
+            contentDescription = "some description here",
+            contentScale = ContentScale.Fit,
             modifier = Modifier
-                .align(Alignment.Center) // keep the image centralized into the Box
                 .graphicsLayer(
-                    // adding some zoom limits (min 50%, max 200%)
-                    scaleX = maxOf(.5f, minOf(3f, scale.value)),
-                    scaleY = maxOf(.5f, minOf(3f, scale.value)),
-                ),
-            contentDescription = null,
-            painter = painterResource(tamtam)
+                    scaleX = zoom,
+                    scaleY = zoom,
+                    translationX = offsetX,
+                    translationY = offsetY,
+                )
+                .fillMaxSize()
         )
     }
 
-//
-//    Box(
+
+    // BOX 쓰기 씷으면 이거 사용
+
+//    Image(
+//        painter = painterResource(id = tamtam),
+//        contentDescription = "some description here",
+//        contentScale = ContentScale.Fit,
 //        modifier = Modifier
-//            .fillMaxSize()
-//            .padding(16.dp)
-//            .pointerInput(Unit) {
-//                detectTransformGestures { _, pan, zoom, _ ->
-//                    scale.floatValue *= zoom
-//                    if (scale.floatValue > maxZoom) {
-//                        scale.floatValue = maxZoom
-//                    } else if(scale.floatValue < minZoom){
-//                        scale.floatValue = minZoom
-//                    }
-//                    offsetX.floatValue += pan.x
-//                    offsetY.floatValue += pan.y
-//                }
-//
-//            }
-//    ) {
-//            Image(painter = painterResource(id = tamtam)
-//                , contentDescription = "탬탬버린"
-//                , modifier = Modifier
-//                    .fillMaxSize()
-//                    .scale(scale.floatValue)
-//                    .offset(offsetX.floatValue.dp, offsetY.floatValue.dp)
+//            .graphicsLayer(
+//                scaleX = zoom,
+//                scaleY = zoom,
+//                translationX = offsetX,
+//                translationY = offsetY,
 //            )
-//    }
+//            .pointerInput(Unit) {
+//                detectTransformGestures(
+//                    onGesture = { _, pan, gestureZoom, _ ->
+//                        zoom = (zoom * gestureZoom).coerceIn(minScale, maxScale)
+//                        if (zoom > 1) {
+//                            val maxOffsetX = imageWidth + 50f
+//                            val maxOffsetY = imageHeight
+//                            offsetX += pan.x * zoom
+//                            offsetY += pan.y * zoom
+//                            offsetX = offsetX.coerceIn(-maxOffsetX, maxOffsetX)
+//                            offsetY = offsetY.coerceIn(-maxOffsetY, maxOffsetY)
+//                        } else {
+//                            offsetX = 0f
+//                            offsetY = 0f
+//                        }
+//                    }
+//                )
+//            }
+//            .fillMaxSize()
+//    )
+
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     ZoomTheme {
