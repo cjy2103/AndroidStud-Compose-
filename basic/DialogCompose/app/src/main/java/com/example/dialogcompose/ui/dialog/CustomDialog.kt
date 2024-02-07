8160503a8168d00c26fef043d0855395a46db02f
S120916\최재영 <cjy2103@naver.com>
2024-02-07 14:53:40 +0900

    저장

--- basic/DialogCompose/app/src/main/java/com/example/dialogcompose/ui/dialog/CustomDialog.kt
+++ basic/DialogCompose/app/src/main/java/com/example/dialogcompose/ui/dialog/CustomDialog.kt
@@ -2,6 +2,7 @@ package com.example.dialogcompose.ui.dialog
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,41 +11,90 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import com.example.dialogcompose.R
 import com.example.dialogcompose.ui.theme.DialogComposeTheme
 
 @Composable
-fun CustomDialog(){
+fun CustomDialog(
+    title: String,
+    message: String = "",
+    positiveText: String,
+    negativeText: String,
+    onClickOk: () -> Unit,
+    onClickNo: () -> Unit
+){
     Row(
         modifier = Modifier
             .fillMaxWidth()
             .height(50.dp)
             .background(
-                LinearGradient(
-                    colors = listOf(Color.White, Color.Cyan),
-                    start = Alignment.TopCenter,
-                    end = Alignment.BottomCenter
+                brush = Brush.horizontalGradient(
+                    colors = listOf(
+                        Color(0xFF94E6E9),
+                        Color.Cyan
+                    )
                 )
             )
     ) {
-        Spacer(modifier = Modifier.width(50.dp))
+        Spacer(modifier = Modifier.width(70.dp))
+
         Image(
             painterResource(id = R.drawable.blue_archive),
             contentDescription = "블루아카이브")
-    }
-}
 
+        Spacer(modifier = Modifier.width(20.dp))
+
+        val mapleFont = FontFamily(
+            Font(R.font.font_maple)
+        )
+
+        val mapleLight = FontFamily(
+            Font(R.font.maplestory_light)
+        )
+
+        Text(
+            modifier = Modifier.align(Alignment.CenterVertically),
+            text = title,
+            fontSize = 24.sp,
+            fontFamily = mapleFont,
+            )
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 60.dp, start = 10.dp, end = 10.dp)
+    ) {
+        Text(text = message)
+    }
+
+}
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun CustomDialogPreView() {
     DialogComposeTheme {
-        CustomDialog()
+        CustomDialog(
+            "블루아카이브",
+            "나쁜 선생에게는 파봉만준다.",
+            "확인",
+            "취소",
+            {},
+            {}
+        )
     }
 }
