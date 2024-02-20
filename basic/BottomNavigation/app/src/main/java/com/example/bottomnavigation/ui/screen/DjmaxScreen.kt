b4bb249a44660380a4eb0d4330401570fc446122
S120916\최재영 <cjy2103@naver.com>
2024-02-20 13:56:45 +0900

    버튼 테두리 추가

--- basic/BottomNavigation/app/src/main/java/com/example/bottomnavigation/ui/screen/DjmaxScreen.kt
+++ basic/BottomNavigation/app/src/main/java/com/example/bottomnavigation/ui/screen/DjmaxScreen.kt
@@ -1,16 +1,29 @@
 package com.example.bottomnavigation.ui.screen
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,6 +41,8 @@ fun DjmaxScreen(modifier: Modifier = Modifier){
 
         Spacer(modifier = Modifier.height(100.dp))
 
+        val buttonClicked = remember { mutableStateOf(true) }
+
         Image(painter = painterResource(id = R.drawable.iv_djmax_x_mas),
             contentDescription = "Djmax",
             modifier = Modifier.size(250.dp,250.dp)
@@ -37,6 +52,24 @@ fun DjmaxScreen(modifier: Modifier = Modifier){
         Spacer(modifier = Modifier.height(20.dp))
 
         Text(text = "Djmax 화면")
+
+        Spacer(modifier = Modifier.height(50.dp))
+
+
+
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .size(150.dp,50.dp)
+            ,
+            border = BorderStroke(2.dp,Color.Black),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFFA8EAE7),
+                contentColor = Color.Black),
+        ) {
+            Text(text = "Button")
+        }
+
 
 
     }
