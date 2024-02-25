9607a868861a08213a089aa1f07aecc409ea706f
S120916\최재영 <cjy2103@naver.com>
2024-02-25 16:01:11 +0900

    Multi Module 예제 작성중

---
+++ basic/Module/data/src/main/java/com/example/data/CharacterData.kt
@@ -0,0 +1,5 @@
+package com.example.data
+
+data class CharacterData(val title : Int, val describe : Int,
+                         val image: Int, val youtubeLink : Int)
+
---
+++ basic/Module/data/src/main/java/com/example/data/CharacterProvider.kt
@@ -0,0 +1,37 @@
+package com.example.data
+
+import android.annotation.SuppressLint
+import androidx.annotation.RawRes
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+
+class CharacterProvider {
+    private val list = ArrayList<CharacterData>()
+
+    @SuppressLint("MutableCollectionMutableState")
+    private val characterList: State<ArrayList<CharacterData>> = mutableStateOf(list)
+
+    init {
+
+        addItem(com.example.moduledivide.R.string.baknana, com.example.moduledivide.R.string.bak_describe, com.example.moduledivide.R.drawable.baknana, com.example.moduledivide.R.string.baknana_link)
+        addItem(R.string.djmax, R.string.djmax_describe, R.drawable.djmax_clear_fail, R.string.djmax_archive)
+        addItem(
+            R.string.djmax_falling_love,
+            R.string.djmax_falling_love_describe,
+            R.drawable.djmax_falling_in_love,
+            R.string.djmax_falling_love_link
+        )
+        addItem(R.string.mwamwa, R.string.mwamwa_describe, R.drawable.mwama,R.string.mwamwa_link)
+        addItem(R.string.tamtam, R.string.tamtam_describe, R.drawable.tamtam,R.string.tamtam_link)
+
+    }
+
+    private fun addItem(title: Int, describe: Int, image: Int, link: Int) {
+        val character = CharacterData(title, describe, image, link)
+        list.add(character)
+    }
+
+    fun getCharacterList(): State<ArrayList<CharacterData>> {
+        return characterList
+    }
+}
