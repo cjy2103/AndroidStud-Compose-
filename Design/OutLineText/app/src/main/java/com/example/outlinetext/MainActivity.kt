f7095b678b241563f948c863c5f2f8e803f01abf
S120916\최재영 <cjy2103@naver.com>
2024-02-27 13:03:26 +0900

    저장

--- Design/OutLineText/app/src/main/java/com/example/outlinetext/MainActivity.kt
+++ Design/OutLineText/app/src/main/java/com/example/outlinetext/MainActivity.kt
@@ -3,21 +3,30 @@ package com.example.outlinetext
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.StrokeJoin
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.outlinetext.ui.text.TextOutLine
 import com.example.outlinetext.ui.theme.OutLineTextTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,32 +48,36 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(modifier: Modifier = Modifier) {
-
     val text = "Text OutLine"
 
-    Text(
-        text = text,
-        modifier = modifier ,
-        style = MaterialTheme.typography.displayLarge.merge(
-            TextStyle(
-                color = Color(0xFF000000),
-                drawStyle = Stroke(
-                    width = 5f
-                )
-            )
-        )
+    val mapleFont = FontFamily(
+        Font(R.font.font_maple)
     )
 
-    Text(
-        text = text,
-        color = Color(0xFF5BCAD8),
-        style = MaterialTheme.typography.displayLarge
-    )
+    Column(modifier.fillMaxWidth().height(200.dp)) {
+        Box(
+            modifier
+                .fillMaxWidth()
+                .height(150.dp)
+                .background(Color(0xFF00BCD4))
+                .padding(top = 30.dp),
+            contentAlignment = Alignment.Center
+        ) {
+
+            TextOutLine(text = text, innerColor = Color(0xFFFFEB3B)
+                , outlineColor = Color(0xFF000000),5f)
+
+        }
+    }
+
+
+
+
 
 
 }
 
-@Preview(showBackground = true, showSystemUi = true)
+@Preview(showBackground = true, widthDp = 400, heightDp = 760)
 @Composable
 fun GreetingPreview() {
     OutLineTextTheme {
--- Design/OutLineText/app/src/main/java/com/example/outlinetext/ui/text/OutLineText.kt
+++ Design/OutLineText/app/src/main/java/com/example/outlinetext/ui/text/OutLineText.kt
@@ -1,6 +1,8 @@
 package com.example.outlinetext.ui.text
 
 import android.graphics.Paint.Style
+import android.graphics.drawable.LayerDrawable
+import android.util.Size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -8,24 +10,61 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
 
 @Composable
-fun TextOutLine(text : String, innerColor : Color, outlineColor : Color){
+fun TextOutLine(text : String, innerColor : Color, outlineColor : Color,
+                strokeWidth : Float){
     Text(
         text = text,
         style = MaterialTheme.typography.displayLarge.merge(
             TextStyle(
                 color = outlineColor,
                 drawStyle = Stroke(
-                    width = 5f
+                    width = strokeWidth
                 )
             )
-        )
+        ),
+        fontSize = 24.sp
     )
 
     Text(
         text = text,
         color = innerColor,
         style = MaterialTheme.typography.displayLarge
+        ,        fontSize = 24.sp
+
     )
+}
+
+@Composable
+fun FontOutLine(text : String, innerColor : Color, outlineColor : Color,
+                font : FontFamily,strokeWidth : Float, size : TextUnit){
+    Text(
+        text = text,
+        style = TextStyle(
+            color = Color(0xFF000000),
+            drawStyle = Stroke(
+                width = strokeWidth
+            )
+        ),
+        fontFamily = font,
+        fontSize = size
+    )
+
+    Text(
+        text = text,
+        color = Color(0xFF5BCAD8),
+        style = TextStyle(
+            color = Color(0xFF000000),
+            drawStyle = Stroke(
+                width = 0f
+            )
+        ),
+        fontFamily = font,
+        fontSize = size
+    )
+
 }
