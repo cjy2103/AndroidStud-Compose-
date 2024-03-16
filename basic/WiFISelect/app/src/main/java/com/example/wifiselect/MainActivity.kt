55897b7d00c420824cf70dd1d88cae1664363a56
S120916\최재영 <cjy2103@naver.com>
2024-03-16 09:02:11 +0900

    WIFI 검증중

--- basic/WiFISelect/app/src/main/java/com/example/wifiselect/MainActivity.kt
+++ basic/WiFISelect/app/src/main/java/com/example/wifiselect/MainActivity.kt
@@ -1,15 +1,36 @@
 package com.example.wifiselect
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.net.wifi.WifiInfo
+import android.net.wifi.WifiManager
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import com.example.wifiselect.ui.theme.WiFISelectTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +43,51 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
     }
 }
 
+@SuppressLint("MissingPermission")
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(modifier: Modifier = Modifier) {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        val context = LocalContext.current
+        Button(
+            onClick = {
+                val intent = Intent(Settings.Panel.ACTION_WIFI)
+                context.startActivity(intent)
+            },
+            modifier = Modifier
+                .padding(top = 100.dp)
+                .size(width = 150.dp, height = 40.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF56E1F3),
+                contentColor = Color.Black
+            )
+        ) {
+            Text(text = "Wi-Fi 목록")
+        }
+
+    }
+
 }
 
-@Preview(showBackground = true)
+@Composable
+fun showWifiList(){
+
+}
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     WiFISelectTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
