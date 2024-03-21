729c06649e87aa36eeb6d50e078e0209327e5888
S120916\최재영 <cjy2103@naver.com>
2024-03-22 08:41:31 +0900

    RetrofitWebServer 기초 UI 설정

--- basic/RetrofitWebServer/app/src/main/java/com/example/retrofitwebserver/MainActivity.kt
+++ basic/RetrofitWebServer/app/src/main/java/com/example/retrofitwebserver/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.retrofitwebserver
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.retrofitwebserver.ui.theme.RetrofitWebserverTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
@@ -30,17 +37,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.fillMaxSize()){
+        Text(
+            modifier = modifier.padding(top = 30.dp),
+            text = "test"
+        )
+
+        Button(onClick = {/* TODO */} ,
+            modifier = modifier
+                .padding(top = 30.dp)
+                .size(width = 150.dp, height = 40.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF56E1F3),
+                contentColor = Color.Black
+            )
+            ){
+            Text(text = "데이터 호출")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     RetrofitWebserverTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
