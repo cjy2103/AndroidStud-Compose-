a04fec7ce73e2f29a97e2d09634b6e5251855b06
S120916\최재영 <cjy2103@naver.com>
2024-03-28 15:35:16 +0900

    커스텀 스위치 작성 완

--- Design/CustomSwitch/app/src/main/java/com/example/customswitch/MainActivity.kt
+++ Design/CustomSwitch/app/src/main/java/com/example/customswitch/MainActivity.kt
@@ -6,16 +6,25 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.customswitch.ui.component.CustomSwitch
 import com.example.customswitch.ui.theme.CustomSwitchTheme
 
@@ -38,8 +47,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.fillMaxSize()) {
-        CustomSwitch(checked = false)
+    var value by remember { mutableStateOf(false) }
+    Column(modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(modifier = modifier.padding(top = 30.dp),
+            text = if(value) "스위치 On" else "스위치 Off",
+            fontSize = 24.sp
+            )
+
+        Spacer(modifier = modifier.height(40.dp))
+
+        CustomSwitch(value, {value = !value})
     }
 }
 
