bde4881c75607381dd99681cc87cca28120f6566
S120916\최재영 <cjy2103@naver.com>
2024-04-01 18:00:32 +0900

    리스트에 띄우기 성공

--- basic/WiFISelect/app/src/main/java/com/example/wifiselect/MainActivity.kt
+++ basic/WiFISelect/app/src/main/java/com/example/wifiselect/MainActivity.kt
@@ -1,37 +1,38 @@
 package com.example.wifiselect
 
 import android.annotation.SuppressLint
-import android.content.Context
-import android.net.wifi.ScanResult
-import android.net.wifi.WifiManager
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
 import com.example.wifiselect.ui.data.WifiInfo
+import com.example.wifiselect.ui.data.WifiInfoProvider
 import com.example.wifiselect.ui.theme.WiFISelectTheme
 
 class MainActivity : ComponentActivity() {
@@ -64,54 +65,15 @@ fun Greeting(modifier: Modifier = Modifier) {
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        
-        
-        Text(text = text)
-        
+
+        val wifiList = remember {
+            mutableStateListOf<WifiInfo>()
+        }
+
         Button(
             onClick = {
-                val wifiManager = context.getSystemService(Context.WIFI_SERVICE) as WifiManager
-                val wifiList = wifiManager.scanResults
-
-                val wifiInfo = wifiManager.connectionInfo
-
-                Log.d("Wi-Fi 정보", "SSID: ${wifiInfo.ssid}")
-                Log.d("Wi-Fi 정보", "BSSID: ${wifiInfo.bssid}")
-                Log.d("Wi-Fi 정보", "신호 강도: ${wifiInfo.rssi}")
-                Log.d("Wi-Fi 정보", "연결 속도: ${wifiInfo.linkSpeed}")
-                Log.d("Wi-Fi 정보", "IP 주소: ${wifiInfo.ipAddress}")
-                Log.d("Wi-Fi 정보", "MAC 주소: ${wifiInfo.macAddress}")
-                Log.d("Wi-Fi 정보", "보안 유형: ${wifiInfo.currentSecurityType}")
-
-                text = wifiInfo.bssid
-
-                // 로그 출력
-                for (wifi in wifiList) {
-                    Log.d("testtest",
-                        WifiInfo(
-                            wifi.SSID,
-                            wifi.BSSID,
-                            wifi.level,
-                            wifi.capabilities,
-                            wifi.frequency
-                        ).toString())
-                }
-
-                val filteredList = ArrayList<ScanResult>()
-
-                val groupedBySsid = wifiList.groupBy { it.SSID }
-
-                for ((_, networks) in groupedBySsid) {
-                    // RSSI가 가장 높은 항목 선택
-                    val maxLevelNetwork = networks.maxByOrNull { it.level }
-                    if (maxLevelNetwork != null) {
-                        filteredList.add(maxLevelNetwork)
-                    }
-                }
-
-                Log.v("결과는?", filteredList.toString())
-                Log.v("길이?", filteredList.size.toString())
-
+                wifiList.clear()
+                wifiList.addAll(WifiInfoProvider().filerList(context))
             },
             modifier = Modifier
                 .padding(top = 40.dp)
@@ -124,31 +86,37 @@ fun Greeting(modifier: Modifier = Modifier) {
             Text(text = "Wi-Fi 목록")
         }
 
+        WiFiList(wifiList)
+
     }
 }
 
-
-@SuppressLint("MissingPermission")
-fun showWifiList(context : Context){
-    Log.v("먼데","222")
-    val wifiManager = context.getSystemService(Context.WIFI_SERVICE) as WifiManager
-    wifiManager.startScan()
-    val result: List<ScanResult> = wifiManager.scanResults
-
-    val descendingList = result.sortedByDescending { it.level }
-    descendingList.forEachIndexed { index, data ->
-        if (data.SSID != "") {
-            Log.d("testtest",
-                WifiInfo(
-                    data.SSID,
-                    data.BSSID,
-                    data.level,
-                    data.capabilities,
-                    data.frequency
-                ).toString())
+@Composable
+fun WiFiList(wifiList: SnapshotStateList<WifiInfo>) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentPadding = PaddingValues(horizontal = 20.dp, vertical = 20.dp)
+    ) {
+        items(wifiList.size) { index ->
+            WifiItem(wifiInfo = wifiList[index])
         }
     }
 }
+
+@Composable
+fun WifiItem(wifiInfo: WifiInfo) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(50.dp)
+            .border(1.dp, Color(0xFFBABABA))
+    ) {
+        Text(text = "SSID: ${wifiInfo.SSID}")
+    }
+
+}
+
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
--- basic/WiFISelect/app/src/main/java/com/example/wifiselect/ui/data/WifiInfoProvider.kt
+++ basic/WiFISelect/app/src/main/java/com/example/wifiselect/ui/data/WifiInfoProvider.kt
@@ -1,14 +1,81 @@
 package com.example.wifiselect.ui.data
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.net.wifi.WifiManager
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 
-class WifiInfoProvider {
-    private val list = ArrayList<WifiInfo>()
+class WifiInfoProvider() {
 
-    @SuppressLint("MutableCollectionMutableState")
-    private val wifiList : MutableState<ArrayList<WifiInfo>> = mutableStateOf(list)
+    fun filerList(context: Context): ArrayList<WifiInfo> {
+        val wifiManager = context.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        @SuppressLint("MissingPermission")
+        val wifiList = wifiManager.scanResults
+
+        val wifiInfo = wifiManager.connectionInfo
+
+        Log.d("Wi-Fi 정보", "SSID: ${wifiInfo.ssid}")
+        Log.d("Wi-Fi 정보", "BSSID: ${wifiInfo.bssid}")
+        Log.d("Wi-Fi 정보", "신호 강도: ${wifiInfo.rssi}")
+        Log.d("Wi-Fi 정보", "연결 속도: ${wifiInfo.linkSpeed}")
+        Log.d("Wi-Fi 정보", "IP 주소: ${wifiInfo.ipAddress}")
+        Log.d("Wi-Fi 정보", "MAC 주소: ${wifiInfo.macAddress}")
+//        Log.d("Wi-Fi 정보", "보안 유형: ${wifiInfo.currentSecurityType}")
+
+        // 로그 출력
+        for (wifi in wifiList) {
+            Log.d("testtest",
+                WifiInfo(
+                    wifi.SSID,
+                    wifi.BSSID,
+                    wifi.level,
+                    wifi.capabilities,
+                    wifi.frequency
+                ).toString())
+            wifi.capabilities
+        }
+
+        var filteredWifiList = ArrayList<WifiInfo>()
+
+
+        for (wifi in wifiList) {
+            val existingWifiInfo = filteredWifiList.find { it.SSID == wifi.SSID }
+            if (existingWifiInfo != null) {
+                if (wifi.level > existingWifiInfo.level) {
+                    filteredWifiList.remove(existingWifiInfo)
+                    filteredWifiList.add(
+                        WifiInfo(
+                            wifi.SSID,
+                            wifi.BSSID,
+                            wifi.level,
+                            wifi.capabilities,
+                            wifi.frequency
+                        )
+                    )
+                }
+            } else {
+                filteredWifiList.add(
+                    WifiInfo(
+                        wifi.SSID,
+                        wifi.BSSID,
+                        wifi.level,
+                        wifi.capabilities,
+                        wifi.frequency
+                    )
+                )
+            }
+        }
+
+        Log.v("결과는?", filteredWifiList.toString())
+        Log.v("길이?", filteredWifiList.size.toString())
+
+        return filteredWifiList
+
+    }
 
 
 
