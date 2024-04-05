2454516e00f468c841de73b01650b956bf1a7c83
S120916\최재영 <cjy2103@naver.com>
2024-04-05 17:07:04 +0900

    다이얼로그 작성 준비

---
+++ basic/WiFISelect/app/src/main/java/com/example/wifiselect/ui/dialog/CustomDialog.kt
@@ -0,0 +1,15 @@
+package com.example.wifiselect.ui.dialog
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CustomDialog(
+    title: String,
+    message: String = "",
+    positiveText: String,
+    negativeText: String,
+    onClickOk: () -> Unit,
+    onClickNo: () -> Unit
+) {
+
+}
